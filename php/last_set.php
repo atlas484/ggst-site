8ca00efce303314f2018451657c2c0bfee6337ae
Atlas <atlasaero484@gmail.com>
2022-08-20 18:20:37 +0000

    added PDO::FETCH_ASSOC to remove duplicate rows

--- php/last_set.php
+++ php/last_set.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         include('/var/private/ggst/db_connect.php');
 
         $sql = "SELECT date, opponent, character_1, character_2, played, won FROM $last_sets_table WHERE last_set IS TRUE LIMIT 1";
-        $response = $db->query($sql)->fetch();
+        $response = $db->query($sql)->fetch(PDO::FETCH_ASSOC);
         
         if (gettype($response) == "array") {
             echo json_encode($response + ["successful" => True]);
