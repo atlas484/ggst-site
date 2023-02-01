ae54370dc2af2baab5482b5115a8e078bd27626f
Atlas <atlasaero484@gmail.com>
2023-01-31 22:53:29 -0600

    template files to build other pages off of

---
+++ src/pages/stats/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavBar from '../../components/NavBar.vue'
+import HomePage from '../../components/HomePage.vue'
+</script>
+
+<template>
+  <NavBar pageName="Stats"/>
+
+  <HomePage />
+</template>
+
+<style>
+
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { join, parse, resolve } from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  alias: {
+    "~": __dirname,
+  },
+  build: {
+    rollupOptions: {
+      input: entryPoints(
+        "index.html",
+        "input.html",
+        "stats.html",
+        "rating.html",
+        "calculator.html",
+      ),
+    },
+  },
+};
+
+function entryPoints(...paths) {
+  const entries = paths.map(parse).map(entry => {
+    const { dir, base, name, ext } = entry;
+    const key = join(dir, name);
+    const path = resolve(__dirname, dir, base);
+    return [key, path];
+  });
+  
+  const config = Object.fromEntries(entries);
+  console.log(config)
+  return config;
+}
